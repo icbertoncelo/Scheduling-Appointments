fafd519278d9bec1da62c926b1b5e3ab7d9715a8
Ian Carlos <carlos.ian007@gmail.com>
2019-05-27 15:44:36 -0300

    Feat(session): Add session controller

---
+++ app/Controllers/Http/SessionController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class SessionController {
+  async store ({ request, response, auth }) {
+    try {
+      const { email, password } = request.all()
+      const token = await auth.attempt(email, password)
+
+      return token
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Ivalid Credentials' } })
+    }
+  }
+}
+
+module.exports = SessionController
