7894fc88ec4e95f6cb279a916e95e9bb33ea7d52
Ian Carlos <carlos.ian007@gmail.com>
2019-05-27 21:38:27 -0300

    Feat(user): Add user update

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Hash = use('Hash')
 
 class UserController {
   async store ({ request }) {
@@ -9,6 +10,45 @@ class UserController {
     const user = await User.create(data)
 
     return user
+  }
+
+  async update ({ request, response, auth: { user } }) {
+    const data = request.only([
+      'username',
+      'old_password',
+      'new_password',
+      'new_password_confirmation'
+    ])
+
+    if (data.old_password) {
+      const isSame = await Hash.verify(data.old_password, user.password)
+
+      if (!isSame) {
+        return response.status(401).send({
+          error: { message: 'Old Password is incorrect' }
+        })
+      }
+
+      if (!data.new_password) {
+        return response.status(401).send({
+          error: { message: 'New password is required' }
+        })
+      }
+
+      if (data.new_password !== data.new_password_confirmation) {
+        return response.status(401).send({
+          error: { message: 'Passwords do not match' }
+        })
+      }
+
+      user.merge({ username: data.username, password: data.new_password })
+
+      await user.save()
+
+      return user
+    } else {
+      return response.status(401).send({ error: { message: 'No password provided' } })
+    }
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -3,4 +3,6 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+Route.put('users', 'UserController.update').middleware('auth')
+
 Route.post('sessions', 'SessionController.store')
